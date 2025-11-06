765d32d0265db929254ff212380605629f69acba
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:34:44 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:34:05.296Z
-Commit: Add dark mode support
+Timestamp: 2025-11-06T00:34:43.685Z
+Commit: Patch security vulnerability in authentication
