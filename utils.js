01c7bdd46f8993a5647ab7c6dd7a31360c074563
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:50:53 +0300

    Introduce real-time notifications

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:36:19.789Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2025-11-06T00:50:52.253Z
+Commit: Introduce real-time notifications
