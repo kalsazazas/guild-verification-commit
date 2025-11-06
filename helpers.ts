697af10f4f7a0e36cf038842a32c1e7af593efc2
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:50:49 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:50:39.385Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:50:48.456Z
+Commit: Resolve null pointer exception in user service
