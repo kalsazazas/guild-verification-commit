d0912275dce873dbb6a2e3d9ae93004dc041827b
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:30:07 +0300

    Introduce real-time notifications

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:29:22.352Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:30:06.653Z
+Commit: Introduce real-time notifications
