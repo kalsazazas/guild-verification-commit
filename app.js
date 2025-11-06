da3abba93edf7fb00284a9cacf10e5ffec7b37b4
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:52:17 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:52:12.136Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:52:16.218Z
+Commit: Fix CSS styling bug on mobile devices
