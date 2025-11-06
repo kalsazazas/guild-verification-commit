dcadbc0ee0d1539ff75cb806c29bd48ce2d00fc5
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:54:28 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:54:14.587Z
-Commit: Add multi-language support
+Timestamp: 2025-11-06T00:54:26.752Z
+Commit: Resolve timezone issues in date handling
