049534f87ba529e06f6146228340ec1687bdc089
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:35:49 +0300

    Fix CSS styling bug on mobile devices

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:35:44.052Z
-Commit: Fix broken links in documentation
+Timestamp: 2025-11-06T00:35:48.333Z
+Commit: Fix CSS styling bug on mobile devices
