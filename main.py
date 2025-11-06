aa87048a37d8f6f92127b3bcd09b03532a869b18
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:54:15 +0300

    Add multi-language support

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:54:08.901Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2025-11-06T00:54:14.587Z
+Commit: Add multi-language support
