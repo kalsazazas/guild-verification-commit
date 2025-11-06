cc516827b0c88321c185d6afe8416a28f3cf2ffb
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:35:37 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:35:21.594Z
-Commit: Introduce real-time notifications
+Timestamp: 2025-11-06T00:35:35.708Z
+Commit: Fix CSS styling bug on mobile devices
