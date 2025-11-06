722fdcd09e74b31019b8126041835a6fa21e4e8d
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:30:03 +0300

    Resolve timezone issues in date handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:29:44.412Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:30:02.271Z
+Commit: Resolve timezone issues in date handling
