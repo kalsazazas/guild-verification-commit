91a1fe068b6b38a3cf352f617f6567545a69621e
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:52:23 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:52:16.218Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-11-06T00:52:21.842Z
+Commit: Fix broken links in documentation
