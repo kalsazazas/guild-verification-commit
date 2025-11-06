4ebb3fc158def8ba8c591c412df853461fb865b4
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:32:59 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:32:50.148Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T00:32:58.013Z
+Commit: Resolve null pointer exception in user service
