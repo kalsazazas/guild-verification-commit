0e062d7534e2ebdbbdef6c7330bcbbac2b8a5cf7
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:36:07 +0300

    Fix broken links in documentation

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T00:35:58.590Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2025-11-06T00:36:06.642Z
+Commit: Fix broken links in documentation
