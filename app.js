673e7c8793d824b41d5bba859b15636baab7b5b2
karo <113212535+kalsazazas@users.noreply.github.com>
2025-11-06 03:29:34 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-01T17:22:29.887Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2025-11-06T00:29:33.447Z
+Commit: Fix broken links in documentation
